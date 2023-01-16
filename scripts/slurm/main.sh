fca17a8fc48ed1dd7af4540ae69eadfe31072b82
Matthew Turner <maturner01@gmail.com>
2023-01-15 21:24:58 -0800

    all supplemental analyses run with reproducible slurm scripts

--- scripts/slurm/main.sh
+++ scripts/slurm/main.sh
@@ -3,6 +3,8 @@
 # Submit main results jobs, one line for each innovation starting condition,
 # specified in the third positional argument to run_helper.sh: 
 # minority group (1), majority group (2), or both groups (Both).
-sbatch --array=1-10 scripts/slurm/run_helper.sh 0.05 1.2 1 1000
-sbatch --array=1-10 scripts/slurm/run_helper.sh 0.05 1.2 2 1000
-sbatch --array=1-10 scripts/slurm/run_helper.sh 0.05 1.2 Both 1000
+#
+# The first positional argument passed to this script is the CSV output write directory.
+sbatch --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 1 1000
+sbatch --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 2 1000
+sbatch --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 Both 1000
