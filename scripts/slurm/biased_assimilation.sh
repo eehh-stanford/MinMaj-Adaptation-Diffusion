99256432da846d17ca5e5ab95875ec50e8c804d0
Matthew Turner <maturner01@gmail.com>
2023-09-14 12:36:34 -0700

    updated biased assimilation slurmer

--- scripts/slurm/biased_assimilation.sh
+++ scripts/slurm/biased_assimilation.sh
@@ -1,16 +1,16 @@
-sbatch --job-name=ba --output=test_ba.out scripts/slurm/run_helper.sh $1 0.05 1.2 1 10 0.25
-# sbatch --job-name=ba --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 1 10 0.25
-# sbatch --job-name=ba --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 2 1000 0.25
-# sbatch  --job-name=ba --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 Both 1000 0.25
+# sbatch --job-name=ba --output=test_ba.out scripts/slurm/run_helper.sh $1 0.05 1.2 1 100 0.25
+sbatch --job-name=ba --output=test_ba.out --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 1 10 0.25
+sbatch --job-name=ba --output=test_ba.out --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 2 1000 0.25
+sbatch  --job-name=ba --output=test_ba.out --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 Both 1000 0.25
 
-# sbatch --job-name=ba --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 1 1000 0.5
-# sbatch --job-name=ba --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 2 1000 0.5
-# sbatch --job-name=ba --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 Both 1000 0.5
+sbatch --job-name=ba --output=test_ba.out --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 1 1000 0.5
+sbatch --job-name=ba --output=test_ba.out --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 2 1000 0.5
+sbatch --job-name=ba --output=test_ba.out --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 Both 1000 0.5
 
-# sbatch --job-name=ba --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 1 1000 0.75
-# sbatch --job-name=ba --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 2 1000 0.75
-# sbatch --job-name=ba --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 Both 1000 0.75
+sbatch --job-name=ba --output=test_ba.out --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 1 1000 0.75
+sbatch --job-name=ba --output=test_ba.out --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 2 1000 0.75
+sbatch --job-name=ba --output=test_ba.out --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 Both 1000 0.75
 
-# sbatch --job-name=ba --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 1 1000 0.95
-# sbatch --job-name=ba --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 2 1000 0.95
-# sbatch --job-name=ba --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 Both 1000 0.95
+sbatch --job-name=ba --output=test_ba.out --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 1 1000 0.95
+sbatch --job-name=ba --output=test_ba.out --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 2 1000 0.95
+sbatch --job-name=ba --output=test_ba.out --array=1-10 scripts/slurm/run_helper.sh $1 0.05 1.2 Both 1000 0.95
