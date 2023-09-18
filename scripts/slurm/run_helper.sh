fcb65208f2dd0bd5f05d785da3d1ba9cc9b14909
Matt Turner <maturner01@gmail.com>
2023-09-18 12:50:59 -0700

    continued refactor starting to replace 'group_1' to min_group, etc.

--- scripts/slurm/run_helper.sh
+++ scripts/slurm/run_helper.sh
@@ -12,4 +12,4 @@ module --force purge
 module load devel
 module load julia/1.9
 
-julia scripts/run_trials.jl $1 --nreplicates=100 --group_1_frac=$2 --a_fitness=$3 --group_w_innovation=$4 --nagents=$5
+julia scripts/run_trials.jl $1 --nreplicates=100 --min_group_frac=$2 --a_fitness=$3 --group_w_innovation=$4 --nagents=$5
