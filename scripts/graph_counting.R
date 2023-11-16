5f4cf20dc7a4d61301712c90567c10e03d373162
Matthew Turner <maturner01@gmail.com>
2023-11-16 10:16:32 -0800

    updated probability/network counting script

--- scripts/graph_counting.R
+++ scripts/graph_counting.R
@@ -2,6 +2,14 @@ library(ggplot2)
 library(data.table)
 library(purrrlyr)
 
+
+mytheme = theme(axis.line = element_line(), legend.key=element_rect(fill = NA),
+                text = element_text(size=22),# family = 'PT Sans'),
+                legend.key.width = unit(2, 'cm'),
+                legend.key.size = unit(1.5, 'lines'),
+                panel.background = element_rect(fill = "white"))
+
+
 choose_nodes_w_replacement <- function(n_nodes, n_edges) {
   return (choose(n_nodes + n_edges - 1, n_edges))
 }
@@ -34,13 +42,31 @@ minmaj_model_probability_minority_has <- function(N, m, kbar, hom) {
 }
 
 
-plot_over_N_homophily <- function(N = c(30, 100), m = seq(0.05, 0.5, 0.05), kbar = 4, 
-                                  h = c(0.0, 0.25, 0.5)){
-  return (
-    CJ(N, m, kbar, h) %>%
-    by_row(function (r) minmaj_model_probability_minority_has(r$N, r$m, r$kbar, r$h),
+plot_over_N_homophily <- function(N = c(30, 100, 200), m = seq(0.05, 0.5, 0.05), kbar = 4, 
+                                  homophily = c(0.0, 0.5), write_dir = "figures"){
+  
+  # Generate data to plot...
+  plot_data <-
+    # ...first create Cartesian product ("cross join") of parameters,...
+    CJ(N, m, kbar, homophily) %>%
+    # ...then calculate probability a given minority node teaches at least one majority agent.
+    by_row(function (r) minmaj_model_probability_minority_has(r$N, r$m, r$kbar, r$homophily),
            .to = "prob", .collate = "cols")
-  )
+  
+  plot_data$N <- as.factor(plot_data$N)
+  plot_data$homophily <- as.factor(plot_data$homophily)
+  
+  ggplot(plot_data, aes(x=m, y=prob)) + 
+    geom_line(aes(linetype=N, color=homophily), size=1) +
+    # geom_point(aes(color=h)) +
+    scale_linetype_manual(values=c("twodash", "dashed", "solid")) +
+    xlab("Minority fraction, " ~ italic("m")) + 
+    ylab("Probability of edge to majority") +
+    mytheme
+  
+  save_path <- file.path(write_dir, "prob_min_teaches_maj.pdf")
+  
+  ggsave(save_path, width = 7.5, height = 5.15)
 }
 
 
